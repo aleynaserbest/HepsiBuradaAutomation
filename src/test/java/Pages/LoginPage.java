f9bcb0d5d37361074098dc0fba0c3824d733eb0f
Aleyna Serbest <80920419+aleynaserbest@users.noreply.github.com>
2023-07-10 09:25:07 +0300

    Update LoginPage.java

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class LoginPage extends Page {
     public final String email = "txtUserName";
     public final String loginBtn = "btnLogin";
-    public final String password = "txtPassword";
-    public final String btnLogin = "//button[@id='btnEmailSelect']";
-
+    
     public LoginPage(WebDriver driver) {
         super(driver);
     }
